e248719763e4020d48b298fbf1b556675b31f4c0
Rishab Raj <rr0948555@gmail.com>
2026-04-19 19:09:50 +0530

    Function

--- Functions/app.js
+++ Functions/app.js
@@ -10,9 +10,20 @@ function myFun2(message,name,val) {
     return val1;
 };
 
-// myFun1();
+function myFunc3(a,b,c){
+    const val1 = a * b / c;
+    console.log('===='+val1);
+    return val1;
+}
+
+myFun1();
 myFun2('Hello','Tovino',100);
 myFun2('Hii','Basil',500);
 
 const val1 = myFun2('Welcome','Jane',1000);
 console.log(val1);
+
+const a1 = myFunc3(4,6,2);
+const a2 = myFunc3(40,60,2);
+const a3 = myFunc3(50,2,25);
+console.log(a1,a2,a3);
