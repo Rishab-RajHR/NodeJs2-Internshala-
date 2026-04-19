3d52b8c918e82ef87d3ac66e07c3183f5ce3dcc9
Rishab Raj <rr0948555@gmail.com>
2026-04-19 19:25:37 +0530

    Function Expression

--- Functions/app2.js
+++ Functions/app2.js
@@ -1,4 +1,4 @@
-// Function Expression is not hoisted
+// Function Expression is not hoisted(Immediately invoked)
 
 const fun1 = function(a,b,c){
     const val = a * b * c;
@@ -7,3 +7,25 @@ const fun1 = function(a,b,c){
 
 const val1 = fun1(5,6,7);
 console.log(val1);
+
+
+// IIFE(Immediately invoked)
+const a = 5;
+const b = 6;
+const c = 7;
+
+(function(a,b,c) {
+    const val = a * b * c;
+    console.log(val);
+})(a,b,c);  // IIFE
+
+
+// Function Declaration is hoisted
+const val2 = fun2(8,2,4);
+
+console.log(val2);
+
+function fun2(a,b,c){
+     const val = a * b * c;
+     return val;
+}
