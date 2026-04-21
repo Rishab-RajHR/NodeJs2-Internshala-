90bc95e00408517abf638cf35c3f3eb87d56e997
Rishab Raj <rr0948555@gmail.com>
2026-04-21 19:52:30 +0530

    Operators

---
+++ Operators/app2.js
@@ -0,0 +1,34 @@
+const first = "Hello";
+const second = "World";
+
+console.log(first + ' ' + second);
+
+
+// Comparison Operator
+let a = "5";
+let b = 5;
+
+const val1 = (a == b);
+const val2 = (a == b);
+console.log(val1);  // true because of type Coercion
+console.log(val2);  // false because do not perform type Coercion
+
+
+
+let c = "8";
+let d = 8;
+// comparison
+let val = (a === b);
+val = (a >= b);
+val = (a !== b);
+
+
+console.log(val);
+
+// logical operators
+
+val4 = (true)&&(false); // both true to be true
+val4 = (false)||(false); // one has to be true to be true
+val4 = !(true); // opposite 
+
+console.log(val4);
