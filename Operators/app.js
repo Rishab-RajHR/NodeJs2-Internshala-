9f1835630c01aa731e169186c26205d4536adc88
Rishab Raj <rr0948555@gmail.com>
2026-04-21 19:36:26 +0530

    Operator

--- Operators/app.js
+++ Operators/app.js
@@ -2,4 +2,8 @@ let val = 100;
 val = 1000;
 val = val + 1;  // val += 1 , val++ all are same
 val = val - 1;  // val -= 1 , val-- all are same
+val = val * 2;  // val *= 2
+val = val / 3;  // val /= 3
+console.log(val*10)
+
 console.log(val);
