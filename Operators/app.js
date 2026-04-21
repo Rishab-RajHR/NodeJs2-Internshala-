d98df7abe5639690ec1f023751564efa4c434b92
Rishab Raj <rr0948555@gmail.com>
2026-04-21 19:42:10 +0530

    Operators

--- Operators/app.js
+++ Operators/app.js
@@ -4,6 +4,24 @@ val = val + 1;  // val += 1 , val++ all are same
 val = val - 1;  // val -= 1 , val-- all are same
 val = val * 2;  // val *= 2
 val = val / 3;  // val /= 3
+
+
+console.log(401%5);
+console.log(401%2);
+console.log(400/2);
+console.log(400%2);
+
+console.log(3**2);
+
+// Assignment Operator
+
+let val2 = 50;
+val2 = val2 % 10;
+console.log(val2);
+
+const val1 = 50 % 10;
+console.log(val1);
+
 console.log(val*10)
 
 console.log(val);
