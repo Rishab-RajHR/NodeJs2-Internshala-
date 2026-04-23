7ebc9c449a549af0799cb73e063765675dda1b39
Rishab Raj <rr0948555@gmail.com>
2026-04-23 22:24:20 +0530

    Objects

--- Objects/app.js
+++ Objects/app.js
@@ -1,8 +1,22 @@
 const myObj1 = {
      first : "Alex",
      last : "Pandian",
-     age : 40
+     age : 40,
+     a1 : 'test',
+     a2 : 'test2',
+     a3 : 'test3',
+     "test 1" : "wow"
 };
 console.log(myObj1);
+console.clear();
+console.log(myObj1.first + ' ' + myObj1.last); // dot notation
 
-console.log(myObj1.first + ' ' + myObj1.last);
+// bracket notation
+console.log(myObj1['first'] + ' ' + myObj1['last']);
+
+for(let x=1; x<4; x++){
+    console.log('a'+x);
+    console.log(myObj1['a'+x]);
+}
+
+console.log(myObj1['test 1']);
