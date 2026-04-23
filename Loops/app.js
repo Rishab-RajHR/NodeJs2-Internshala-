28e157c02221e606652748a787f3bed9f8eaf35b
Rishab Raj <rr0948555@gmail.com>
2026-04-23 22:03:39 +0530

    Loops

---
+++ Loops/app.js
@@ -0,0 +1,25 @@
+let counter = 10;
+for(let i=0; i<counter; i++){
+    console.log('value of i '+ i);
+}
+
+for(let x=10; x>0; x--){
+    console.log('value of x '+ x);
+}
+
+console.clear();
+
+// While Loop
+let y = 0;
+while (y < 10){
+    console.log('Value of y '+y);
+    y++;
+}
+
+// The Do-While Loop is executed at least once even if the condition is false
+let z = 100;
+do {
+   console.log("DO WHILE value of z "+ z);
+   z++;
+}
+while (z<10);
