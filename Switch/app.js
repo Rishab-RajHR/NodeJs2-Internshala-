652bcb4c71034df2773a428499bc3d435381848e
Rishab Raj <rr0948555@gmail.com>
2026-04-23 21:47:44 +0530

    Switch

---
+++ Switch/app.js
@@ -0,0 +1,12 @@
+// Switch statement evaluates the expression
+
+const val = 'yes';
+
+switch (val) {
+   case 'yes':
+      console.log('It was Yes');
+      console.log('Test');
+      break;
+   case 'no':
+      console.log('It was no');
+}
