cc1b87595047ca842ef94594aa4d11fa4f192853
Rishab Raj <rr0948555@gmail.com>
2026-04-24 19:19:25 +0530

    Object

---
+++ ObjectMethod/app.js
@@ -0,0 +1,11 @@
+const myObj4 = {
+    val : 100,
+    val1 : true,
+    val2 : 'String',
+    val3 : {
+       first : 'Tillu',
+       last : 'Singh'
+    }
+}
+
+console.log(myObj4.val3.first + ' ' + myObj4.val3.last);
