3a2e28c3f673a342876209765c4bdd5675a0bb30
Rishab Raj <rr0948555@gmail.com>
2026-04-24 19:40:25 +0530

    Arrays

--- Arrays/app.js
+++ Arrays/app.js
@@ -1,4 +1,7 @@
 const myArray = ['test',10,true,{pne:'one',two:'two'}];
+const myObj = {one:'one',two:'two'};
 console.log(myArray);
 
 console.log(myArray.length);
+console.log(typeof myObj);
+console.log(typeof myArray);
