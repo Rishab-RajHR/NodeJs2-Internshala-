cab3ad0bbdab865ceff778f405e8ae7285dd0d3b
Rishab Raj <rr0948555@gmail.com>
2026-04-26 19:30:26 +0530

    Method

--- UpdateArrays/Method3.js
+++ UpdateArrays/Method3.js
@@ -2,5 +2,9 @@ const myArr2 = ['one','two','three','two','three','two','three','two','three']
 
 // get the lastIndex
 val = myArr2.lastIndexOf('two')
+console.log(myArr2);
+// Sorting of the data
+myArr2.sort();
+console.log(myArr2);
 
 console.log(val);
