02f9522ab79bd8137eb8c0c840df9d716c61ca1a
Rishab Raj <rr0948555@gmail.com>
2026-04-26 19:40:42 +0530

    Method4

--- UpdateArrays/Method4.js
+++ UpdateArrays/Method4.js
@@ -4,6 +4,16 @@ const myArr2 = ['one','two','three','two','three','two','three','two','three']
 const myArr3 = myArr2.map(val=>{
     return val;
 })
+
+// Another syntax for Map Method
+// const myArr3 = myArr2.map(val=>val);
+
 myArr2[2] = "HELLO";
 console.log(myArr3);
 console.log(myArr2);
+
+const myArr4 = myArr2.map(val => {
+    val += 'TESTED';
+    return val;
+})
+console.log(myArr4);
