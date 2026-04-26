975113ed04dbc38ab803f0f5aa645dadb7389f05
Rishab Raj <rr0948555@gmail.com>
2026-04-26 19:22:38 +0530

    Methods

--- UpdateArrays/Method2.js
+++ UpdateArrays/Method2.js
@@ -1,5 +1,9 @@
 const myArr2 = ['one','two','three'];
 
+// includes => Is it present or not
 val = myArr2.includes('two');
+
+// indexOf => Checks the index value
+val = myArr2.indexOf('three')
 
 console.log(val);
