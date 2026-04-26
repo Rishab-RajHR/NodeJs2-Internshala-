8148e31bf1ce6d9fb0e94b378cb43dc6939d9157
Rishab Raj <rr0948555@gmail.com>
2026-04-26 19:11:04 +0530

    Method

--- UpdateArrays/Method.js
+++ UpdateArrays/Method.js
@@ -3,4 +3,7 @@ const myArr1 = ['one','two',55,true,{one:"one",two:"two"}]
 // Convert to string
 const val = myArr1.toString();
 
-console.log(myArr1);
+// Convert to JSON
+val = JSON.stringify(myArr1);
+
+console.log(val);
