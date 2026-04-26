a775caf03180518c7f52792d07f63958289b58c8
Rishab Raj <rr0948555@gmail.com>
2026-04-26 19:36:56 +0530

    Method

--- UpdateArrays/Method4.js
+++ UpdateArrays/Method4.js
@@ -1,6 +1,9 @@
 const myArr2 = ['one','two','three','two','three','two','three','two','three']
 
+// Map method returns the new Array
 const myArr3 = myArr2.map(val=>{
     return val;
 })
+myArr2[2] = "HELLO";
 console.log(myArr3);
+console.log(myArr2);
