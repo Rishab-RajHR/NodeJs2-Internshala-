d8ea9ee8ce9b43ad1e28066b694f3ade2f18c087
Rishab Raj <rr0948555@gmail.com>
2026-04-27 18:16:34 +0530

    Template Literals

--- TemplateLiterals/app.js
+++ TemplateLiterals/app.js
@@ -1,8 +1,12 @@
 const a = `Hello`;
 const b = `World`;
 
+function add(x,y){
+    return x + y;
+}
+
 const val = `
- ${a} ${b} ${5+5}
+ ${a} ${b} ${5+5} ${add(10,50)}
  single quotes '
  double quotes "
  New \n Line
