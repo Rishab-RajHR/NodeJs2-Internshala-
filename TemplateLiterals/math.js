7a0e7b76ba7532cf4907308f5e8b5bec52f6c879
Rishab Raj <rr0948555@gmail.com>
2026-04-27 18:24:50 +0530

    Math

---
+++ TemplateLiterals/math.js
@@ -0,0 +1,13 @@
+console.log(Math.random());
+
+const ran1 = Math.random()*100;
+console.log(Math.floor(ran1));
+console.log(Math.ceil(ran1));
+
+for(let i=0; i<100; i++){
+    console.log(ranNum(1,10));
+}
+
+function ranNum(min,max){
+    return Math.floor(Math.random()*max)+min;
+}
