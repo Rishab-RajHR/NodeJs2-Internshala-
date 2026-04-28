fd3dccea11bd3899ce8bf6f7835df126b626ef12
Rishab Raj <rr0948555@gmail.com>
2026-04-28 18:43:46 +0530

    App

---
+++ AsynchronousJS/app3.js
@@ -0,0 +1,19 @@
+// console.log(global);
+let a = 'hello';
+let counter = 0;
+function timer(val){
+    console.log(`timer ran ${val} ${a}`);
+}
+
+function message(val){
+    counter++;
+    console.log(`${counter}. - ${val}`);
+}
+
+for(let i=5; i>0; i--){
+    setTimeout(timer,i*1000,`Loop : ${i}`);
+}
+
+console.log('new val');
+
+a = 'World';
