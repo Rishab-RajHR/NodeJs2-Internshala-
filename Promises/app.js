0d68db9dd79b2156b6daf4d9342de1c9d6c161e4
Rishab Raj <rr0948555@gmail.com>
2026-04-29 18:48:44 +0530

    Promises

---
+++ Promises/app.js
@@ -0,0 +1,13 @@
+const pro1 = new Promise((resolve,reject)=>{
+    resolve('success');
+    reject('reject');
+})
+
+pro1.then(
+    (val)=>{
+        console.log(`TRUE : ${val}`);
+    },
+    (err)=>{
+        console.log(`ERROR!~!! : ${err}`)
+    } 
+);
