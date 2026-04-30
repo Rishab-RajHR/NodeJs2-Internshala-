2c6d4735b32498861736582cca8c7497f11be04c
Rishab Raj <rr0948555@gmail.com>
2026-04-30 18:44:36 +0530

    Async Await

--- AsyncAwait/assignment.js
+++ AsyncAwait/assignment.js
@@ -1,4 +1,11 @@
 const friends = [];
 const person1 = {first:"Laurence",last:"Bishnoi"};
+const person2 = {first:"Laurence",last:"Murmu"};
+const person3 = {first:"Tovino",last:"Singh"};
 friends.push(person1);
-console.log(friends);
+friends.push(person2);
+friends.push(person3);
+friends.forEach((person)=>{
+    console.log(person);
+})
+// console.log(friends);
