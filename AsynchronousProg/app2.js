1ed7c959c47553b046442aaaa012b7a712800c1d
Rishab Raj <rr0948555@gmail.com>
2026-05-01 19:29:49 +0530

    Async

---
+++ AsynchronousProg/app2.js
@@ -0,0 +1,17 @@
+const arr = [1,2,3,4,5];
+console.clear();
+arr.forEach((el)=>{
+   console.log(el);
+   console.log('Sync');
+})
+
+function aFun(myArr,callback){
+   myArr.forEach((el)=>{
+      setTimeout(callback,1000);
+   })
+}
+
+aFun(arr,(val)=>{
+    console.log(val);
+    console.log('async');
+})
