2245fac3d711541f3ec10c111d7ab1ad4477bc42
Rishab Raj <rr0948555@gmail.com>
2026-05-01 19:44:42 +0530

    Stack Queue

---
+++ StackQueue/app2.js
@@ -0,0 +1,34 @@
+const arr = [1,2,3,4,5];
+console.clear();
+
+
+function aFun(myArr,callback){
+   myArr.forEach((el)=>{
+      setTimeout(callback,1000,el);
+   })
+   myArr.forEach((el)=>{
+      setTimeout(callback,0,`${el} -`);
+   })
+}
+
+aFun(arr,(val)=>{
+    console.log(val);
+    console.log('async');
+})
+
+console.log('test');
+
+arr.forEach((el)=>{
+   console.log(el);
+   console.log('Sync');
+})
+console.clear();
+console.log('one');
+setTimeout(()=>{
+  console.log('five');
+},2000)
+console.log('two');
+setTimeout(()=>{
+    console.log('four');
+},1000)
+console.log('three');
