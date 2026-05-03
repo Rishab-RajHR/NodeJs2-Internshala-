3bc5aeb1a5139346d49aaf009bc220bef4f6c045
Rishab Raj <rr0948555@gmail.com>
2026-05-03 19:18:57 +0530

    Job Queue

--- EventLoop/JobQueue.js
+++ EventLoop/JobQueue.js
@@ -3,5 +3,19 @@ function test1(){
    return test1();
 }
 console.log('one');
-test1();
+// test1();
 console.log('two');
+
+// Event loop checks the call stack -
+
+console.clear();
+const one = ()=> console.log('one');
+const two = ()=> console.log('two');
+
+const test2 = () => {
+     console.log('Three');
+     one();
+     two();
+}
+
+test2();
