edc904c820f092216aea8a053d32c1be25c477e8
Rishab Raj <rr0948555@gmail.com>
2026-05-05 21:51:23 +0530

    Object Terminal

--- ObjectTerminal/app.js
+++ ObjectTerminal/app.js
@@ -5,6 +5,13 @@ const rLine = require('readline').createInterface({
 
 rLine.question(`What is your name?`, name => {
     console.log(name);
+    // console.log(process);
     console.log(`Welcome ${name}`);
     rLine.close();
 })
+
+process.on('exit',(val)=>{
+    console.log(`Process Exiting ${val}`);
+})
+
+console.log(`Last Message`);
