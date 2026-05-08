fb54bb9990770e559aab2af312856cc62c72050f
Rishab Raj <rr0948555@gmail.com>
2026-05-08 19:38:46 +0530

    Modules

--- Modules/mod1.js
+++ Modules/mod1.js
@@ -3,5 +3,8 @@ console.log(module.filename);
 
 test1.test1();
 console.log(test1);
+console.log(test2);
 const myName = `${test.person.first} ${test.person.last}`;
+const myName2 = `${test.person.first} ${test.person.last}`;
 console.log(myName);
+console.log(myName2);
