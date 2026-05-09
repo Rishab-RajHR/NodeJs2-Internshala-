b044910fc5a60b4c6fdaceb1f7a9cb04aba72cde
Rishab Raj <rr0948555@gmail.com>
2026-05-09 22:41:23 +0530

    Module

--- Modules2/test4.js
+++ Modules2/test4.js
@@ -3,7 +3,7 @@ const val2 = 'Test Private 2';
 const val3 = 'New Message';
 
 function output(message){
-    console.log(`This is your message ${message}.Private values ${val2}`);
+     return `This is your message ${message}. Private value ${val2}`;
 }
 
 module.exports.output = output;
