c4764c8ad3a399966830a0819a8a5c9a8fadf58b
Rishab Raj <rr0948555@gmail.com>
2026-05-10 19:24:37 +0530

    Modules

--- Modules4/mod4.js
+++ Modules4/mod4.js
@@ -1,3 +1,7 @@
 const http = require('http');
 
-console.log(http);
+// console.log(http);
+
+http.createServer((req,res)=>{
+     res.writeHead(200, {'Content-Type':'text/html'});
+}).listen(8000);
