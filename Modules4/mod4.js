e1190408be8e76e778c30ffe514ef4b5b8837b30
Rishab Raj <rr0948555@gmail.com>
2026-05-10 19:28:39 +0530

    Moduels

--- Modules4/mod4.js
+++ Modules4/mod4.js
@@ -4,5 +4,7 @@ const http = require('http');
 
 http.createServer((req,res)=>{
      res.writeHead(200, {'Content-Type':'text/html'});
-     res.end('Hello World');
+     res.write('Hello');
+     res.write('World');
+     res.end();
 }).listen(8080);
