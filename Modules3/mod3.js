d4c8553948c71c2e357d8c3fa95084e12407342e
Rishab Raj <rr0948555@gmail.com>
2026-05-10 19:18:03 +0530

    Modules

--- Modules3/mod3.js
+++ Modules3/mod3.js
@@ -1,8 +1,12 @@
 const person = require('./test5');
 
 const friend1 = new person('Alex', 'Pandian');
+const friend2 = new person('Tovino', 'Thomas');
+const friend3 = new person('Basil', 'Joseph');
 
 console.log(friend1);
 console.log(friend1.first);
 console.log(friend1.last);
 console.log(friend1.myName());
+console.log(friend2.myName());
+console.log(friend3.myName());
