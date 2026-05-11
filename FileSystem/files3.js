8f63f74ab801cceaeea2ea2779ff5a4389c28173
Rishab Raj <rr0948555@gmail.com>
2026-05-11 14:25:23 +0530

    File System

--- FileSystem/files3.js
+++ FileSystem/files3.js
@@ -10,4 +10,13 @@ fs.readdir(dir, (err,files)=>{
     console.log(err);
     tempCounter = files.length + 1;
     newFileName = `new${tempCounter}.html`;
+    createNewFile(newFileName);
 })
+
+function createNewFile(myfileName){
+    const html = `<h1>Hello ${tempCounter} </h1>`;
+    fs.appendFile(dir + '/' + myfileName, html, (err)=>{
+        if(err) throw err;
+        console.log(`Save File ${fileName}`)
+    })
+}
