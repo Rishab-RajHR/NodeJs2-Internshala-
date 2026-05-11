ea64bdfd15af9889834754a408c1ccb811a25a1a
Rishab Raj <rr0948555@gmail.com>
2026-05-11 14:29:14 +0530

    FIleSystem

--- FileSystem/files3.js
+++ FileSystem/files3.js
@@ -4,19 +4,23 @@ let tempCounter = 1;
 let newFileName = `new${tempCounter}.html`;
 console.clear();
 
-fs.readdir(dir, (err,files)=>{
-    console.log(files);
-    console.log(files.length);
+function maker(){
+for(let i=0; i<5; i++){
+fs.readdir(dir, (err,files3)=>{
+    console.log(files3);
+    console.log(files3.length);
     console.log(err);
-    tempCounter = files.length + 1;
+    tempCounter = files3.length + i +  1;
     newFileName = `new${tempCounter}.html`;
     createNewFile(newFileName);
 })
+}
+}
 
 function createNewFile(myfileName){
     const html = `<h1>Hello ${tempCounter} </h1>`;
     fs.appendFile(dir + '/' + myfileName, html, (err)=>{
         if(err) throw err;
-        console.log(`Save File ${fileName}`)
+        console.log(`Save File ${myfileName}`)
     })
 }
