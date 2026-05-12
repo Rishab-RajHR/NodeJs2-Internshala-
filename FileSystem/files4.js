9c1a2d71fc7400f03d7ddb2896a8049bfd3dc6ed
Rishab Raj <rr0948555@gmail.com>
2026-05-12 12:59:10 +0530

    File System

---
+++ FileSystem/files4.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const dir = './public';
+let tempCounter = 1;
+let newFileName = `new${tempCounter}.html`;
+console.clear();
+
+function maker(){
+for(let i=0; i<5; i++){
+fs.readdir(dir, (err,files3)=>{
+    console.log(files3);
+    console.log(files3.length);
+    console.log(err);
+    tempCounter = files3.length + i +  1;
+    newFileName = `new${tempCounter}.html`;
+    createNewFile(newFileName);
+})
+}
+}
+
+function overmyLog(val){
+    const html =` - ${val}`;
+    fs.writeFile(dir + '/log.html',html,(err)=>{
+        if(err) throw err;
+        console.log(`Updated writeFile ${val}`);
+    })
+}
+
+function createNewFile(myfileName){
+    const html = `<h1>Hello ${tempCounter} </h1>`;
+    fs.appendFile(dir + '/' + myfileName, html, (err)=>{
+        if(err) throw err;
+        console.log(`Save File ${myfileName}`)
+    })
+}
