524fae6b6020943bcf49c5d78d87faf2d2a7c45d
Rishab Raj <rr0948555@gmail.com>
2026-05-12 13:09:18 +0530

    Deleting

---
+++ FileSystem/files5.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const dir = './public';
+let tempCounter = 1;
+let newFileName = `new${tempCounter}.html`;
+console.clear();
+
+function maker(){
+for(let i=0; i<5; i++){
+fs.readdir(dir, (err,files3)=>{
+    console.log(files3);
+    console.log(files3.length);
+    console.log(err);
+    tempCounter = files3.length + i +  1;
+    newFileName = `new${tempCounter}.html`;
+    createNewFile(newFileName);
+})
+}
+}
+
+delFile(dir + '/log.html');
+function delFile(val){
+   fs.unlink(val,(err)=>{
+      if(err) throw err;
+      console.log('File Deleted');
+   })
+}
+// myLog('test2');
+
+
+function overmyLog(val){ // overwriting file /create
+    const html =` - ${val}`;
+    fs.writeFile(dir + '/log.html',html,(err)=>{
+        if(err) throw err;
+        console.log(`Updated writeFile ${val}`);
+    })
+}
+
+function myLog(val){  // adding /create
+    const html = ` <br> * ${val}`;
+    fs.appendFile(dir + '/log.html',html,(err)=>{
+        if(err) throw err;
+        console.log(`*Appended ${val}`);
+    })
+}
+
+function createNewFile(myfileName){
+    const html = `<h1>Hello ${tempCounter} </h1>`;
+    fs.appendFile(dir + '/' + myfileName, html, (err)=>{
+        if(err) throw err;
+        console.log(`Save File ${myfileName}`)
+    })
+}
