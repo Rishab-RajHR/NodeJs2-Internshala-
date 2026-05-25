f08cee8483729be741dc2b62ad2c906b5abf80ee
Rishab Raj <rr0948555@gmail.com>
2026-05-25 14:42:57 +0530

    FileSystem

--- FileSystemStats/script.js
+++ FileSystemStats/script.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
 const http = require('http');
 const dir = './public';
+const mainFile = 'new1.html';
+const url = `./${dir}/${mainFile}`;
+console.log(url);
+
+http.createServer((req, res)=>{
+     fs.readFile(url,(err,html)=>{
+        res.writeHead(200,{
+           'Content-Type':'text/html'
+        });
+        res.write("Hello World");
+        return res.end();
+     })
+     console.log('Server running');
+}).listen(8080);
