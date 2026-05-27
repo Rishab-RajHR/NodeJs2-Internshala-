a169d0db60970db6e77ef2ce02de5b70f06ec39e
Rishab Raj <rr0948555@gmail.com>
2026-05-27 22:24:11 +0530

    File

--- FileReadSystem/index.js
+++ FileReadSystem/index.js
@@ -2,3 +2,13 @@ const fs = require('fs');
 const fileName = 'test.txt';
 const data = fs.readFileSync(fileName,'utf8');
 console.log(data);
+
+const arr = ['red','blue','green'];
+
+fs.writeFile(fileName,'\nHello World Again',(err)=>{
+     if(err) {
+        console.log(err);
+     } else{
+        console.log('DONE');
+     }
+})
