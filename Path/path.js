25a48d0a2abb2c22950d2410946cdeb09d98eda0
Rishab Raj <rr0948555@gmail.com>
2026-05-27 22:35:38 +0530

    path

--- Path/path.js
+++ Path/path.js
@@ -4,3 +4,10 @@ const dir = 'public';
 console.clear();
 const url = path.join('/','.',dir.fileName);
 console.log(url);
+
+console.log(path.dirname(url));
+console.log(path.basename(url));
+console.log(path.extname(url));
+
+const info = path.resolve(fileName);
+console.log(info);
