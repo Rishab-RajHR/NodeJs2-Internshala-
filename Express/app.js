b384ecb23a89dcfd409f2d10e9419b577705f1ee
Rishab Raj <rr0948555@gmail.com>
2026-05-31 14:38:08 +0530

    Express

--- Express/app.js
+++ Express/app.js
@@ -2,6 +2,14 @@ const express =  require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello</h1>');
+})
+
+app.get('/test', (req,res)=>{
+    res.send('<h1>Test</h1>')
+})
+
 app.listen(port,()=>{
     console.log(`Ready listening on ${port}`);
 })
