41697bf569155530b5183acd147b0fc815051cf6
Rishab Raj <rr0948555@gmail.com>
2026-06-01 19:48:49 +0530

    Multiple Callbacks

---
+++ MultipleCallback/index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Test</h1>');
+})
+
+app.get('/info',(req,res)=>{
+    res.send('<h1>Info</h1>');
+})
+
+// Pattern Matching
+// app.get('/ab?cd',(req,res)=>{
+//     res.send('<h1>ABC</h1>');
+// })
+
+// app.get('/ab+cd',(req,res)=>{
+//     res.send('<h1>ABCDE</h1>')
+// })
+
+// Reg exp
+// app.get('/z/',(req,res)=>{
+//    res.send('<h1>ZZZZZZZZ</h1>')
+// })
+
+// Route Parameters
+app.get('/users/:id/:val',(req,res,next)=>{
+    res.send(req.params)
+    console.log(req.params.id)
+    next()},
+    (req,res)=>{
+       console.log(`Second callback ${req.params.val}`)
+       next()
+    },(req,res,next)=>{
+        console.log(`Third callback ${req.params.val}`)
+})
+
+app.listen(port,()=>{
+    console.log(`Ready listening on ${port}`);
+})
